0411d1f832383aea02eb3c299c04ad38e17acf65
DiegoEustaquio <79175504+DiegoEustaquio@users.noreply.github.com>
2023-06-22 19:25:27 +0000

    challenge09 done

--- Scripts/challenge07.sql
+++ Scripts/challenge07.sql
@@ -3,15 +3,15 @@
 -- Common table expression (CTE), FORMAT(), strftime(), FORMAT(), GROUP BY, ORDER BY
 
 WITH TotalSalesYear AS (
-    SELECT
-      strftime('%Y', soldDate) AS 'SoldYear',
-      salesAmount
-    FROM sales
+  SELECT
+    strftime('%Y', soldDate) AS 'SoldYear',
+    salesAmount
+  FROM sales
 )
 SELECT
-    SoldYear,
-    FORMAT("$%.2f", sum(salesAmount)) AS 'AnnualSales',
-    count(*) AS 'NumbSales'
+  SoldYear,
+  FORMAT("$%.2f", sum(salesAmount)) AS 'AnnualSales',
+  count(*) AS 'NumbSales'
 FROM TotalSalesYear
 GROUP BY SoldYear
 ORDER BY SoldYear;
--- Scripts/challenge09.sql
+++ Scripts/challenge09.sql
@@ -1,6 +1,12 @@
 -- challenge **
 -- Find all sales where the car purchased was electric
+-- LEFT JOIN, WHERE, WHERE SUBQUERY
 
 
-SELECT *
-FROM model m, inventory
+SELECT sls.salesId, sls.inventoryId, inv.modelId
+FROM sales sls
+LEFT JOIN inventory inv ON inv.inventoryId = sls.inventoryId
+WHERE inv.modelId IN (
+  SELECT modelId
+  FROM model
+  WHERE EngineType = 'Electric');
